a46207da47cf06c5e74d73b5b4eeca3f5b97f3f9
lianxin <1933467270@qq.com>
2021-04-26 17:14:33 +0800

    ci(other): 项目规范
    增加项目提交规范
    
    ISSUES CLOSED: #1

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:验证提交规范
+ * @Version: 2.0
+ * @Autor: Wsl
+ * @Date: 2021-04-26 17:10:37
+ * @LastEditors: Wsl
+ * @LastEditTime: 2021-04-26 17:11:56
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] }
